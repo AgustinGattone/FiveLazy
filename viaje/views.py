4101571123cf964e181d361a1508984848e250c3
agustingattone <agustingattone@gmail.com>
2017-07-06 09:12:55 -0300

    YA ANDA EL TEMPLATE

--- viaje/views.py
+++ viaje/views.py
@@ -6,7 +6,7 @@ from .models import Vuelo
 # Create your views here.
 def lista_viaje(request):
     paquetesvar = Paquete.objects.order_by('nombre')
-    return render(request, 'viaje/static/index.html', {'paquetes': paquetesvar})
+    return render(request, '/home/informatica/Escritorio/fivelazy/FiveLazy/viaje/templates/index.html', {'paquetes': paquetesvar})
 
 def listar_vuelos(request):
     vuelosvar = Vuelo.objects.all()
