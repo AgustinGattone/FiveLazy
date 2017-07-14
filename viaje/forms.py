e4764d2f10eec0199f602cbaa88f662a40f9a846
jereromanoo <jere0699@gmail.com>
2017-07-14 11:07:57 -0300

    cambios en los formularios de comprar, agrego de imagenes y a la base de datos

--- viaje/forms.py
+++ viaje/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from .models import Vuelo
+from .models import Usuario
 class ComprarVueloForm(forms.ModelForm):
 	class Meta:
 		model = Vuelo
-		fields = ('lugar', 'horaYFechaDeEmbarque', 'precio', 'nombreDeVuelo')
+		fields = ('lugar','nombreDeVuelo', 'horaYFechaDeEmbarque')
 	
--- viaje/urls.py
+++ viaje/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^excursiones/', views.lista_excursiones, name="views.lista_excursiones"),
     url(r'^hoteles/', views.lista_hoteles, name="views.lista_hoteles"),
     url(r'^paquetes/', views.lista_paquetes, name="views.lista_paquetes"),
+    url(r'comprar/$', views.comprar, name='views.comprar'),
 
     ]
